ee60bc7921026f7fe4c4042d35f4670ce7fc8373
AbiemAdillah <Abiem.adillah@gmail.com>
2018-04-09 23:21:05 +0800

    Buat Getter and Setter

--- src/praktek03/Tas.java
+++ src/praktek03/Tas.java
@@ -10,4 +10,30 @@ public class Tas {
                            "Kapasitas \t: "+kapasitas+"\n"+
                            "Harga \t: "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getKapasitas() {
+        return kapasitas;
+    }
+
+    public void setKapasitas(String kapasitas) {
+        this.kapasitas = kapasitas;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
