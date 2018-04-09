94a9e996f10b2b23e788a18cc6bec6f2287a2d56
AbiemAdillah <Abiem.adillah@gmail.com>
2018-04-09 23:04:03 +0800

    Buat Method cetakInfo()

--- src/praktek03/Tas.java
+++ src/praktek03/Tas.java
@@ -4,4 +4,10 @@ public class Tas {
     String merk;
     string warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                            "Warna \t"+warna+"\n"+
+                            "Harga \t"+harga);
+    }
 }
