37f6a007474b16c8062a429c94e7a72450632e24
AbiemAdillah <Abiem.adillah@gmail.com>
2018-04-09 23:24:42 +0800

    menggunakan method set

--- src/praktek03/TasAksi.java
+++ src/praktek03/TasAksi.java
@@ -2,10 +2,10 @@ package praktek03;
 public class TasAksi {
     public static void main(String[] args) {
       Tas s = new Tas();
-      s.merk="Eiger";
-      s.kapasitas="25 L";
-      s.harga=300000;
-              
-      s.cetakInfo();
+     s.setMerk("Eiger");
+     s.setKapasitas("25 L");
+     s.setHarga(300000);
+     
+     s.cetakInfo();
     }
 }
