333573f0a0009be2f7513842f14dbc67258d4982
AbiemAdillah <Abiem.adillah@gmail.com>
2018-04-09 23:28:47 +0800

    menggunakan method get

--- src/praktek03/TasAksi.java
+++ src/praktek03/TasAksi.java
@@ -7,5 +7,16 @@ public class TasAksi {
      s.setHarga(300000);
      
      s.cetakInfo();
+     
+     System.out.print("Merknya \t: ");
+     System.out.println(s.getMerk());
+     System.out.print("Kapasitasnya \t: ");
+     System.out.println(s.getKapasitas());
+      System.out.print("Harganya \t: ");
+     System.out.println(s.getHarga());
+    
+    
     }
+    
+ 
 }
